85719dd443699294ebf6fceeb9e29209ceda37ca
Gareth Brickman <brickstick@gmail.com>
2020-10-27 23:20:54 -0400

    Add task 3 file

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -13,11 +13,6 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
