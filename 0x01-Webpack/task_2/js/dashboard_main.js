ce44f93703f5fc39a6db76f1301bf3bac3fd70bf
Gareth Brickman <brickstick@gmail.com>
2020-11-03 20:50:41 -0500

    Use js file from previous task

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,29 +1,17 @@
 import $ from "jquery";
 import _ from 'lodash';
 
-import '../css/main.css';
+$('body').append("<p>Holberton Dashboard</p>");
+$('body').append("<p>Dashboard data for the studnts</p>");
+$('body').append("<button>Click here to get started</button>");
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
 
-$('body').append(
-    `<div id='logo'></div>`,
-    '<p>Holberton Dashboard</p>',
-    '<p>Dashboard data for the students</p>',
-    '<button>Click here to get started</button>',
-    `<p id='count'></p>`,
-    '<p>Copyright - Holberton School</p>'
-);
-
-const counter = () => {
-  let count = 0;
-  return () => {
-    return (count += 1);
-  };
-};
+let count = 0;
   
-const updateCounter = counter();
+function updateCounter() {
+  count = count + 1;
+  $('#count').text(`${count.toString()} clicks on the button`);
+}
 
-const buttonOnClick = () => {
-  const count = updateCounter();
-  $('#count').text(`${count} clicks on the button`);
-};
-
-$('button').on('click', _.debounce(buttonOnClick));
+$('button').on('click', _.debounce(updateCounter));
