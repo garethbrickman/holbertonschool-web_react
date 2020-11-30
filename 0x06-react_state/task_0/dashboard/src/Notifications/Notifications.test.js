537e410f0e81b406299820d20c2a587387c4b777
Gareth Brickman <brickstick@gmail.com>
2020-11-29 22:19:45 -0500

    Add tests for displayDrawer functionality

--- 0x06-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x06-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { expect } from 'chai';
+import { expect as expect2 } from 'chai';
 import { shallow } from 'enzyme';
 import { StyleSheetTestUtils } from 'aphrodite';
 
@@ -18,27 +18,27 @@ describe('Notifications', () => {
   test('renders without crashing', () => {
     const wrapper = shallow(<Notifications />);
 
-    expect(wrapper.exists());
+    expect2(wrapper.exists());
   });
 
   describe('displayDrawer is true', () => {
     test('has a close button', () => {
       const wrapper = shallow(<Notifications displayDrawer={true} />);
   
-      expect(wrapper.find('img')).to.have.lengthOf(1);
+      expect2(wrapper.find('img')).to.have.lengthOf(1);
     });
     test('menu item is displayed', () => {
       const wrapper = shallow(<Notifications displayDrawer={true} />);
       const menuItem = wrapper.find('[data-testid="menu-item"]');
 
-      expect(menuItem).to.have.lengthOf(1);
+      expect2(menuItem).to.have.lengthOf(1);
     });
 
     test('notifications div is displayed', () => {
       const wrapper = shallow(<Notifications displayDrawer={true} />);
       const notifs = wrapper.find('[data-testid="notifs"]');
 
-      expect(notifs).to.have.lengthOf(1);
+      expect2(notifs).to.have.lengthOf(1);
     });
   });
 
@@ -47,14 +47,14 @@ describe('Notifications', () => {
       const wrapper = shallow(<Notifications />);
       const menuItem = wrapper.find('[data-testid="menu-item"]');
 
-      expect(menuItem).to.have.lengthOf(1);
+      expect2(menuItem).to.have.lengthOf(1);
     });
 
     test('notifications div is not displayed', () => {
       const wrapper = shallow(<Notifications />);
       const notifs = wrapper.find('div.Notifications');
 
-      expect(notifs).to.have.lengthOf(0);
+      expect2(notifs).to.have.lengthOf(0);
     });
   });
 
@@ -63,14 +63,14 @@ describe('Notifications', () => {
       const wrapper = shallow(<Notifications />);
       const notifs = wrapper.find(NotificationItem);
 
-      expect(notifs).to.have.lengthOf(0);
+      expect2(notifs).to.have.lengthOf(0);
     });
 
     test('renders correctly if listNotifications prop not specified', () => {
       const wrapper = shallow(<Notifications />);
       const notifs = wrapper.find(NotificationItem);
 
-      expect(notifs).to.have.lengthOf(0);
+      expect2(notifs).to.have.lengthOf(0);
     });
   });
 
@@ -81,8 +81,8 @@ describe('Notifications', () => {
         {id: 2, type: "default", value: "y"}
       ];
       const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={nots} />);
-      expect(wrapper.find("ul")).to.have.lengthOf(1);
-      expect(wrapper.find(NotificationItem)).to.have.lengthOf(2);
+      expect2(wrapper.find("ul")).to.have.lengthOf(1);
+      expect2(wrapper.find(NotificationItem)).to.have.lengthOf(2);
     });
   });
 
@@ -90,29 +90,34 @@ describe('Notifications', () => {
     const wrapper = shallow(<Notifications displayDrawer={true} />);
     const noNewNotifs = wrapper.find('[data-testid="notifs"] p');
 
-    expect(noNewNotifs).to.have.lengthOf(1);
-    expect(noNewNotifs.text()).to.equal('No new notifications for now');
+    expect2(noNewNotifs).to.have.lengthOf(1);
+    expect2(noNewNotifs.text()).to.equal('No new notifications for now');
   });
-  // TODO
-/*   test('markAsRead calls console.log', () => {
-    console.log = jest.fn();
+  test('clicking menu item calls handleDisplayDrawer', () => {
+    const handleDisplayDrawer = jest.fn();
 
-    const wrapper = shallow(<Notifications />);
-    wrapper.instance().markAsRead(0);
-
-    expect(console.log).toHaveBeenCalledWith(
-      `Notification 0 has been marked as read`
+    const wrapper = shallow(
+      <Notifications handleDisplayDrawer={handleDisplayDrawer} />
     );
-  }); */
+
+    const menuItem = wrapper.find('[data-testid="menu-item"]');
+    expect2(menuItem).to.have.lengthOf(1);
+
+    menuItem.simulate('click');
+    expect(handleDisplayDrawer).toHaveBeenCalledTimes(1);
+  });
+
+  test('clicking close button calls hideDisplayDrawer', () => {
+    const handleHideDrawer = jest.fn();
+
+    const wrapper = shallow(
+      <Notifications displayDrawer={true} handleHideDrawer={handleHideDrawer} />
+    );
+
+    const close = wrapper.find('[data-testid="close-notifs"]');
+    expect2(close).to.have.lengthOf(1);
+
+    close.simulate('click');
+    expect(handleHideDrawer).toHaveBeenCalledTimes(1);
+  });
 });
-
-/* test('logOut alerts with correct string', () => {
-  const myLogOut = jest.fn(() => undefined);
-  const myAlert = jest.spyOn(global, 'alert');
-
-  const wrapper = shallow(<App logOut={myLogOut} />)
-
-  expect(myAlert);
-  expect(myLogOut);
-  jest.restoreAllMocks();
-}); */
