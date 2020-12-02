a5e9d8527242c59713a9d0cde812dd5e93353e29
Gareth Brickman <brickstick@gmail.com>
2020-12-01 22:35:44 -0500

    Add file for task 5

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,14 @@
+import immutable from 'immutable';
+
+const { Map, List } = immutable;
+
+export function concatElements(page1, page2) {
+  return List(page1).concat(List(page2));
+}
+
+export function mergeElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+
+  return map1.merge(map2);
+}
