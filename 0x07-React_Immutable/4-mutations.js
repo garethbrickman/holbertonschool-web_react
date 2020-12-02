4d3fe6245e36e84cea251b2cac2a048d52e254eb
Gareth Brickman <brickstick@gmail.com>
2020-12-01 22:21:52 -0500

    Use map.set instead of setIn method

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,18 +1,14 @@
 import immutable from 'immutable';
 
-const { Map, setIn } = immutable;
+const { Map } = immutable;
 
 export const map = Map({
-    1: 'Liam',
-    2: 'Noah',
-    3: 'Elijah',
-    4: 'Oliver',
-    5: 'Jacob',
-    6: 'Lucas',
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
 });
 
-/* console.log(Object.fromEntries([...map])); */
-
-export const map2 = setIn(setIn(map, ['2'], 'Benjamin'), ['4'], 'Oliver');
-
-/* console.log(Object.fromEntries([...map2])); */
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
