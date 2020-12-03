65f890e6d2723bd6074212e82e1923fcd7424b14
Gareth Brickman <brickstick@gmail.com>
2020-12-03 18:05:39 -0500

    Add tests for login/logout functionality using user state as context

--- 0x06-react_state/task_2/dashboard/src/Header/Header.test.js
+++ 0x06-react_state/task_2/dashboard/src/Header/Header.test.js
@@ -4,6 +4,7 @@ import { expect as expect2 } from 'chai';
 import { StyleSheetTestUtils } from 'aphrodite';
 
 import Header from './Header';
+import AppContext from '../App/AppContext';
 
 describe('Header', () => {
   beforeEach(() => {
@@ -46,5 +47,67 @@ describe('Header', () => {
 
     expect2(image.exists());
     expect2(h1.exists());
+  });
+
+  test('logoutSection NOT created if isLoggedIn is false', () => {
+    const wrapper = mount(<Header />, {
+      context: {
+        user: {
+          email: '',
+          password: '',
+          isLoggedIn: false
+        },
+        logOut: () => {}
+      }
+    });
+
+    const logout = wrapper.find('#logoutSection');
+
+    expect2(logout).to.have.lengthOf(0);
+  });
+
+  test('logoutSection created if isLoggedIn is true', () => {
+    const wrapper = mount(<AppContext.Provider
+    value={{
+        user: {
+          email: 'email@email.com',
+          password: 'pass123',
+          isLoggedIn: true
+        },
+        logOut: () => {}
+      }}
+    >
+      <Header />
+      </AppContext.Provider>
+    );
+
+    const logout = wrapper.find('#logoutSection');
+
+    expect2(logout).to.have.lengthOf(1);
+  });
+
+
+  test('clicking logout link calls logOut', () => {
+    const logOut = jest.fn();
+    const wrapper = mount(
+      <AppContext.Provider
+        value={{
+          user: {
+            email: 'email@email.com',
+            password: 'pass123',
+            isLoggedIn: true
+          },
+          logOut
+        }}
+      >
+        <Header />
+      </AppContext.Provider>
+    );
+
+    const logout = wrapper.find('em');
+    expect2(logout).to.have.lengthOf(1);
+
+    logout.simulate('click');
+    expect(logOut).toHaveBeenCalledTimes(1);
   });
 });
