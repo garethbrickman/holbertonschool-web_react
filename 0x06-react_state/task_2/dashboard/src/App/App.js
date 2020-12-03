71e556fe74c2262c045f248c70e97787c3eb81b7
Gareth Brickman <brickstick@gmail.com>
2020-12-03 16:45:04 -0500

    Create login/logout functions, wrap App with AppContext

--- 0x06-react_state/task_2/dashboard/src/App/App.js
+++ 0x06-react_state/task_2/dashboard/src/App/App.js
@@ -10,13 +10,26 @@ import CourseList from '../CourseList/CourseList';
 import BodySection from '../BodySection/BodySection';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 
+import AppContext from './AppContext';
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.handleLogout = this.handleLogout.bind(this);
     this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
-    this.state = {displayDrawer: false};
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+
+    this.state = {
+      displayDrawer: false,
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+      logOut: () => this.logOut(),
+    };
   }
 
   handleDisplayDrawer() {
@@ -27,6 +40,25 @@ class App extends Component {
     this.setState({displayDrawer: false});
 }
 
+  logIn(email, password) {
+    this.setState({
+      user: {
+        email: email,
+        password: password,
+        isLoggedIn: true,
+      },
+    });
+  }
+
+  logOut() {
+    this.setState({
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+    });
+  }
 
   componentDidMount() {
       window.addEventListener("keydown", this.handleLogout);
@@ -45,7 +77,8 @@ class App extends Component {
     }
 
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn } = this.state.user;
+    const {user, logOut} = this.state;
 
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
@@ -65,6 +98,7 @@ class App extends Component {
 
     return (
       <Fragment>
+        <AppContext.Provider value={{ user, logOut }}>
         <Notifications 
         listNotifications={listNotifications}
         displayDrawer={this.state.displayDrawer}
@@ -76,7 +110,7 @@ class App extends Component {
             <div className={css(styles.padding)}>
               {!isLoggedIn && (
                   <BodySectionWithMarginBottom title='Log in to continue'>
-                    <Login />
+                    <Login logIn={this.logIn}/>
                   </BodySectionWithMarginBottom>
                 )}
                 {isLoggedIn && (
@@ -94,20 +128,11 @@ class App extends Component {
               </div>
           <Footer />
         </div>
+        </AppContext.Provider>
       </Fragment>
     );
   }
 }
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
-};
-
-App.defaultProps = {
-  isLoggedIn: false,
-  logOut: () => undefined
-};
 
 const styles = StyleSheet.create({
   'sans-serif': {
