c4b6076a5060a0ba7791e7641f3090d403d9e249
Gareth Brickman <brickstick@gmail.com>
2020-12-16 13:02:48 -0500

    Add suppressStyleInjection for aphrodite to prevent errors during testing

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/Footer/Footer.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/Footer/Footer.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 import Footer from './Footer';
 import AppContext from '../App/AppContext';
 
 describe('Footer', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+  
   test('renders without crashing', () => {
     const wrapper = shallow(<Footer />);
 
